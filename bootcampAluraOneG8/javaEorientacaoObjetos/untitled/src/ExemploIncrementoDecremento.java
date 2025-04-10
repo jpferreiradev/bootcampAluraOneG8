f53fdbd01477c99bb8e685edf0613b5be21dc485
JP Ferreira <jotepauder01@gmail.com>
2025-04-09 21:49:19 -0300

    Começando a segunda parte do bootcamp com Java

---
+++ bootcampAluraOneG8/javaEorientacaoObjetos/untitled/src/ExemploIncrementoDecremento.java
@@ -0,0 +1,20 @@
+public class ExemploIncrementoDecremento {
+    public static void main(String[] args) {
+
+
+        // Exemplo pre-incremento
+        int x = 10;
+        int resultado = ++x;
+        System.out.println(x);
+        System.out.println(resultado);
+
+        System.out.println();
+
+        // Exemplo pos-incremento
+        int y = 5;
+        int resultado2 = y++;
+        System.out.println(y);
+        System.out.println(resultado2);
+
+    }
+}
